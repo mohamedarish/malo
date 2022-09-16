bd2fee9f072c7e6dbc27ee41753bfef345acb454
Mohamed Arish <arish1248@gmail.com>
2022-09-16 22:31:47 +0530

    Yeah Baby, Second Commit 🫡

--- src/convertor/convert.ts
+++ src/convertor/convert.ts
@@ -1,15 +1,15 @@
-// import { vowels, consonants, compounds, chil, modifiers } from "./data";
+import { vowels, consonants, compounds, modifiers, chillaksharam } from "./data";
 
 const en2ml = (word: string): string[] => {
 	const res: string[] = [];
 
-	// for (let i = 0; i < word.length; i += 1) {
-
-	// }
+	for (let i = 0; i < word.length; i += 1) {
+		
+	}
 
 	return res;
 };
 
-// console.log(en2ml("maaman"));
+// console.log(en2ml("omana"));
 
 export default en2ml;
--- src/convertor/data.ts
+++ src/convertor/data.ts
@@ -154,5 +154,205 @@ const compounds: letters[] = [
     }
 ];
 
-export default { vowels, compounds };
+const consonants: letters[] = [
+    {
+        english: "k",
+        malayalam: ["ക"]
+    },
+    {
+        english: "kh",
+        malayalam: ["ഖ"]
+    },
+    {
+        english: "g",
+        malayalam: ["ഗ"]
+    },
+    {
+        english: "gh",
+        malayalam: ["ഘ"]
+    },
+    {
+        english: "ng",
+        malayalam: ["ങ"]
+    },
+    {
+        english: "ch",
+        malayalam: ["ച"]
+    },
+    {
+        english: "chh",
+        malayalam: ["ഛ"]
+    },
+    {
+        english: "j",
+        malayalam: ["ജ"]
+    },
+    {
+        english: "jh",
+        malayalam: ["ഝ"]
+    },
+    {
+        english: "nj",
+        malayalam: ["ഞ"]
+    },
+    {
+        english: "t",
+        malayalam: ["ട"]
+    },
+    {
+        english: "dt",
+        malayalam: ["ഠ"]
+    },
+    {
+        english: "d",
+        malayalam: ["ഡ"]
+    },
+    {
+        english: "dd",
+        malayalam: ["ഢ"]
+    },
+    {
+        english: "n",
+        malayalam: ["ണ", "ന"]
+    },
+    {
+        english: "th",
+        malayalam: ["ത"]
+    },
+    {
+        english: "thh",
+        malayalam: ["ഥ"]
+    },
+    {
+        english: "dh",
+        malayalam: ["ദ"]
+    },
+    {
+        english: "dhh",
+        malayalam: ["ധ"]
+    },
+    {
+        english: "p",
+        malayalam: ["പ"]
+    },
+    {
+        english: "ph",
+        malayalam: ["ഫ"]
+    },
+    {
+        english: "b",
+        malayalam: ["ബ"]
+    },
+    {
+        english: "bh",
+        malayalam: ["ഭ"]
+    },
+    {
+        english: "m",
+        malayalam: ["മ"]
+    },
+    {
+        english: "y",
+        malayalam: ["യ"]
+    },
+    {
+        english: "r",
+        malayalam: ["ര", "റ"]
+    },
+    {
+        english: "l",
+        malayalam: ["ല", "ള"]
+    },
+    {
+        english: "v",
+        malayalam: ["വ"]
+    },
+    {
+        english: "sh",
+        malayalam: ["ശ", "ഷ"]
+    },
+    {
+        english: "s",
+        malayalam: ["സ"]
+    },
+    {
+        english: "h",
+        malayalam: ["ഹ"]
+    },
+    {
+        english: "zh",
+        malayalam: ["ഴ"]
+    }
+];
 
+const chillaksharam: letters[] = [
+    {
+        english: "l",
+        malayalam: ["ൽ", "ൾ"]
+    },
+    {
+        english: "n",
+        malayalam: ["ൺ", "ൻ"]
+    },
+    {
+        english: "r",
+        malayalam: ["ർ"]
+    },
+    {
+        english: "m",
+        malayalam: ["ം"]
+    }
+];
+
+const modifiers: letters[] = [
+    {
+        english: ".",
+        malayalam: ["്"]
+    },
+    {
+        english: "aa",
+        malayalam: ["ാ"]
+    },
+    {
+        english: "i",
+        malayalam: ["ി"]
+    },
+    {
+        english: "ee",
+        malayalam: ["ീ"]
+    },
+    {
+        english: "u",
+        malayalam: ["ു"]
+    },
+    {
+        english: "oo",
+        malayalam: ["ൂ"]
+    },
+    {
+        english: "ru",
+        malayalam: ["ൃ"]
+    },
+    {
+        english: "e",
+        malayalam: ["െ", "േ"]
+    },
+    {
+        english: "ai",
+        malayalam: ["ൈ"]
+    },
+    {
+        english: "o",
+        malayalam: ["ൊ", "ോ"]
+    },
+    {
+        english: "ou",
+        malayalam: ["ൗ"]
+    },
+    {
+        english: "au",
+        malayalam: ["ൗ"]
+    }
+];
+
+export default { vowels, compounds, consonants, chillaksharam, modifiers };
