4c87df55472c320f1572ac9c2a032673b3c2a9fe
Mohamed Arish T P <arish1248@gmail.com>
2022-09-21 21:06:26 +0530

    zero-minus always settle

--- src/content.ts
+++
@@ -1,5 +0,0 @@
-const script = document.createElement("script");
-script.setAttribute("type", "module");
-script.setAttribute("src", browser.runtime.getURL("./dist/scripts/displayMeanings.js"));
-const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
-head.insertBefore(script, head.lastChild);
