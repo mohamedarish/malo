2f5c66dcf5a49018b3e46b790cc4f02f6d02bd8e
akishtp <makishtp@gmail.com>
2022-09-26 16:49:49 +0530

    simham is shown on extention now🦁

--- src/popup_scripts/popup.ts
+++ src/popup_scripts/popup.ts
@@ -5,7 +5,24 @@ document.addEventListener("DOMContentLoaded", async () => {
         meanings: meangings[];
     };
 
+    var the_word = document.getElementById("the-word")!;
+    the_word.innerHTML = "Word: " + meanings.meanings[0].word;
+
     console.log("content_script: ", meanings.meanings);
 
-    document.body.innerHTML = meanings.meanings[0].meaning[0];
+    for (let i = 0; i < meanings.meanings.length; i++) {
+        for (let j = 0; j < meanings.meanings[i].meaning.length; j++) {
+            var el = document.createElement("li"),
+                content = document.createTextNode(
+                    meanings.meanings[i].meaning[j]
+                ),
+                myUl = document.getElementById("list-items");
+
+            el.appendChild(content);
+            el.id = "list-item";
+            if (myUl != null) {
+                myUl.appendChild(el);
+            }
+        }
+    }
 });
