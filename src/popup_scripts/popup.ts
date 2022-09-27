e8df57d92d5779161d8e0fa31a1a07bb56e3b615
Mohamed Arish T P <arish1248@gmail.com>
2022-09-27 22:11:37 +0530

    The code had a few standard errors and also used alot of innerHTML which has been replaced with innerText or appendChild

--- src/popup_scripts/popup.ts
+++ src/popup_scripts/popup.ts
@@ -5,15 +5,18 @@ document.addEventListener("DOMContentLoaded", async () => {
         meanings: meangings[];
     };
 
-    var the_word = document.getElementById("the-word")!;
-    the_word.innerHTML = "Word: " + meanings.meanings[0].word;
+    const the_word = document.getElementById("the-word");
+
+    if (!the_word) return;
+
+    the_word.innerText = "Word: " + meanings.meanings[0].word;
 
     console.log("content_script: ", meanings.meanings);
 
     if (meanings.meanings[0].meaning.length === 0) {
-        var el = document.createElement("li"),
+        const el = document.createElement("li"),
             myUl = document.getElementById("list-items");
-        el.innerHTML += "അർത്ഥം കണ്ടെത്താൻ കഴിയുന്നില്ല";
+        el.innerText += "അർത്ഥം കണ്ടെത്താൻ കഴിയുന്നില്ല";
         el.id = "list-item";
         if (myUl != null) {
             myUl.appendChild(el);
@@ -21,24 +24,31 @@ document.addEventListener("DOMContentLoaded", async () => {
     } else {
         for (let i = 0; i < meanings.meanings.length; i++) {
             for (let j = 0; j < meanings.meanings[i].meaning.length; j++) {
-                var el = document.createElement("li"),
+                const el = document.createElement("li"),
                     btn = document.createElement("button"),
                     content = document.createTextNode(
                         meanings.meanings[i].meaning[j]
                     ),
                     myUl = document.getElementById("list-items");
 
-                btn.innerHTML += '<img src="../icons/copy-light.png">';
+                const img = document.createElement("img");
+                img.setAttribute("src", "../icons/copy-light.png");
+
+                btn.appendChild(img);
+
                 btn.id = "item-button";
                 btn.addEventListener("click", function copyButtonFn() {
                     navigator.clipboard.writeText(
                         meanings.meanings[i].meaning[j]
                     );
-                    document.getElementById("alert")!.classList.add("visible");
+                    const alert = document.getElementById("alert");
+
+                    if (!alert) return;
+
+                    alert.classList.add("visible");
+
                     setTimeout(() => {
-                        document
-                            .getElementById("alert")!
-                            .classList.remove("visible");
+                        alert.classList.remove("visible");
                     }, 1500);
                 });
                 el.appendChild(content);
