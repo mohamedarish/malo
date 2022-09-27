51c87a36f5880a84df5398a221e116d70e717d6c
akishtp <makishtp@gmail.com>
2022-09-27 12:49:22 +0530

    അർത്ഥം കണ്ടെത്താൻ കഴിയുന്നില്ല🤷‍♀️

--- src/popup_scripts/popup.ts
+++ src/popup_scripts/popup.ts
@@ -10,32 +10,43 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     console.log("content_script: ", meanings.meanings);
 
-    for (let i = 0; i < meanings.meanings.length; i++) {
-        for (let j = 0; j < meanings.meanings[i].meaning.length; j++) {
-            var el = document.createElement("li"),
-                btn = document.createElement("button"),
-                content = document.createTextNode(
-                    meanings.meanings[i].meaning[j]
-                ),
-                myUl = document.getElementById("list-items");
+    if (meanings.meanings[0].meaning.length === 0) {
+        var el = document.createElement("li"),
+            myUl = document.getElementById("list-items");
+        el.innerHTML += "അർത്ഥം കണ്ടെത്താൻ കഴിയുന്നില്ല";
+        el.id = "list-item";
+        if (myUl != null) {
+            myUl.appendChild(el);
+        }
+    } else {
+        for (let i = 0; i < meanings.meanings.length; i++) {
+            for (let j = 0; j < meanings.meanings[i].meaning.length; j++) {
+                var el = document.createElement("li"),
+                    btn = document.createElement("button"),
+                    content = document.createTextNode(
+                        meanings.meanings[i].meaning[j]
+                    ),
+                    myUl = document.getElementById("list-items");
 
-            // btn.innerHTML += "<div>Ayyayyoo</div>";
-            btn.innerHTML += '<img src="../icons/copy-light.png">';
-            btn.id = "item-button";
-            btn.addEventListener("click", function copyButtonFn() {
-                navigator.clipboard.writeText(meanings.meanings[i].meaning[j]);
-                document.getElementById("alert")!.classList.add("visible");
-                setTimeout(() => {
-                    document
-                        .getElementById("alert")!
-                        .classList.remove("visible");
-                }, 1000);
-            });
-            el.appendChild(content);
-            el.appendChild(btn);
-            el.id = "list-item";
-            if (myUl != null) {
-                myUl.appendChild(el);
+                btn.innerHTML += '<img src="../icons/copy-light.png">';
+                btn.id = "item-button";
+                btn.addEventListener("click", function copyButtonFn() {
+                    navigator.clipboard.writeText(
+                        meanings.meanings[i].meaning[j]
+                    );
+                    document.getElementById("alert")!.classList.add("visible");
+                    setTimeout(() => {
+                        document
+                            .getElementById("alert")!
+                            .classList.remove("visible");
+                    }, 1500);
+                });
+                el.appendChild(content);
+                el.appendChild(btn);
+                el.id = "list-item";
+                if (myUl != null) {
+                    myUl.appendChild(el);
+                }
             }
         }
     }
